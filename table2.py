c6ba45774cec2f4016a1e84c3688d25e9b812f88
Makale Lord <110202582+MakaleLord@users.noreply.github.com>
2022-07-28 18:32:26 -0500

    Add files via upload

---
+++ table2.py
@@ -0,0 +1,74 @@
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
+html{
+   display:flex;
+   justify-content:center;
+   align-items: center;
+   width: 100%;
+   height: 100%;
+   background-image: url('https://source.unsplash.com/collection/327760/1600x900');
+   background-size:cover;
+}
+
+body{
+   width:55%;
+   height:60vh;
+   border:1px solid lightgray;
+   background-color:white;
+   border-radius:8px;
+   font-family: 'Roboto Mono', monospace;
+   box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+   border-top:32px solid #eee;
+   overflow-y:scroll;
+}
+
+body > div{
+  height: calc(100% - 40px);
+  overflow-y:auto;
+  padding: 20px;
+  font-size: 24px;
+  white-space: pre;
+}
+
+pre{
+  margin:inherit;
+  font-family: inherit;
+}
+</style>
+
+<div>
+ 13 x 0 = 0
+ 13 x 1 = 13
+ 13 x 2 = 26
+ 13 x 3 = 39
+ 13 x 4 = 52
+ 13 x 5 = 65
+ 13 x 6 = 78
+ 13 x 7 = 91
+ 13 x 8 = 104
+ 13 x 9 = 117
+
+ 32 x 0 = 0
+ 32 x 1 = 32
+ 32 x 2 = 64
+ 32 x 3 = 96
+ 32 x 4 = 128
+ 32 x 5 = 160
+ 32 x 6 = 192
+ 32 x 7 = 224
+ 32 x 8 = 256
+ 32 x 9 = 288
+
+ 23 x 0 = 0
+ 23 x 1 = 23
+ 23 x 2 = 46
+ 23 x 3 = 69
+ 23 x 4 = 92
+ 23 x 5 = 115
+ 23 x 6 = 138
+ 23 x 7 = 161
+ 23 x 8 = 184
+ 23 x 9 = 207
+
+Total is:  4950
